7407c7499c5f4409d4acf2398e5fb200b32380db
umepy <haruya04183083@gmail.com>
2023-02-01 19:21:24 +0900

    impl: mhsa 0.1

--- image-classification-baselines/models/vision_transformer.py
+++ image-classification-baselines/models/vision_transformer.py
@@ -34,16 +34,32 @@ class VitInputLayer(nn.Module):
             torch.Tensor: (B, N, D) N is number of tokens. D is size of embedding dim.
         """
         x = self.patch_emb_layer(x)  # (B, D, H/P, W/P) P is size of patch
-        x = x.flatten(2)  # (B, D, Np)
+        x = x.flatten(2)  # (B, D, Np) Np is number of patches
         x = x.transpose(1, 2)  # (B, Np, D)
         cls_tokens = self.cls_token.repeat(x.size(0), 1, 1)
         x = torch.cat((cls_tokens, x), dim=1)
-        x = x + self.pos_emb
+        x = x + self.pos_emb  # (B, Np+1, D)
         return x
 
 
-class SelfAttention(nn.Module):
-    pass
+class MultiHeadSelfAttention(nn.Module):
+    def __init__(self, emb_dim: int = 384, head: int = 8) -> None:
+        super(MultiHeadSelfAttention, self).__init__()
+        self.emb_dim = emb_dim
+        self.head = head
+        self.query = nn.Linear(emb_dim, emb_dim, bias=False)
+        self.key = nn.Linear(emb_dim, emb_dim, bias=False)
+        self.value = nn.Linear(emb_dim, emb_dim, bias=False)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        assert len(x.size()) == 3  # (B, Np+1, D)
+        assert x.size(2) % self.head == 0
+
+        # multi-head split
+        x = x.reshape(
+            x.size(0),
+            x.size(1),
+        )
 
 
 if __name__ == "__main__":
