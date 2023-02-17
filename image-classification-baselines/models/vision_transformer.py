ee23bdb898fa3d0ca5b26a99d0cd907d8e675895
umepy <haruya04183083@gmail.com>
2023-02-17 14:17:36 +0900

    implemented: VIT

--- image-classification-baselines/models/vision_transformer.py
+++ image-classification-baselines/models/vision_transformer.py
@@ -113,15 +113,34 @@ class VitEncoderBlock(nn.Module):
 
 
 class Vit(nn.Module):
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        in_channel: int = 3,
+        num_classes: int = 10,
+        emb_dim: int = 384,
+        num_patch_row: int = 2,
+        image_size: int = 32,
+        num_blocks: int = 7,
+        head: int = 8,
+        hidden_dim: int = 384 * 4,
+        dropout: float = 0.0,
+    ) -> None:
         super(Vit, self).__init__()
+
+        self.input_layer = VitInputLayer(in_channel, emb_dim, num_patch_row, image_size)
+        self.encoder = nn.Sequential(*[VitEncoderBlock(emb_dim, head, hidden_dim, dropout) for _ in range(num_blocks)])
+        self.mlp_head = nn.Sequential(nn.LayerNorm(emb_dim), nn.Linear(emb_dim, num_classes))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.input_layer(x)
+        x = self.encoder(x)
+        cls_token = x[:, 0]
+        pred = self.mlp_head(cls_token)
+        return pred
 
 
 if __name__ == "__main__":
     x = torch.randn((2, 3, 32, 32))
-    vitin = VitInputLayer()
-    vitenc = VitEncoderBlock()
-    x = vitin(x)
-    print(x.size())
-    x = vitenc(x)
-    print(x.size())
+    model = Vit()
+    pred = model(x)
+    print(pred)
